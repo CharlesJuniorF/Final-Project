33f07e3f4171857d357e530a08bacb2ab3c2edf5
CharlesJuniorF <161252667+CharlesJuniorF@users.noreply.github.com>
2025-11-25 17:22:27 -0500

    Camera fixed

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -95,8 +95,9 @@ public class Player : MonoBehaviour
 
     void RotateCamera()
     {
-        float horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
-        transform.Rotate(0, horizontalRotation, 0);
+        float horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity * 5;
+        Debug.Log(horizontalRotation);
+        transform.Rotate(0, 0+horizontalRotation, 0);
 
         verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
         verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
